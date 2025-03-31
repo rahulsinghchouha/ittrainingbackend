b963782f7c3635b209a6e53a7c740c76ca620f06
rahulsinghchouha <singhrahul15737@gmail.com>
2025-03-31 18:48:24 +0530

    student data updated

--- middleware/session.js
+++ middleware/session.js
@@ -1,6 +1,6 @@
 exports.isAuthenticated = async (req, res, next) => {
       console.log("inside the cookies", req.session);
-    const token = req.session.token; // Ensure 'token' matches the name used in res.cookie
+      const token = req.session.token; // Ensure 'token' matches the name used in res.cookie
 
     if (token) {
         console.log("token found", token);
